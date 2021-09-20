71b251c44dc37903261e23afcae576aa18f76d29
Ramzi Ben Younes <benyounesramzii@gmail.com>
2021-09-20 15:44:49 +0200

    addToCart improvement

--- font-end/scripts/product.js
+++ font-end/scripts/product.js
@@ -1,3 +1,5 @@
+/*** AFFICHAGE DE L'ARTICLE SÉLECTIONNÉ ***/
+
 // Déclaration variables pour accéder à l'argument "id" de l'API
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
@@ -26,10 +28,7 @@ function getCameras() {
 
       // Formatage du prix pour l'afficher en euros
       article.price = article.price / 100;
-      productCardPrice.innerText = new Intl.NumberFormat("fr-FR", {
-        style: "currency",
-        currency: "EUR",
-      }).format(article.price);
+      productCardPrice.innerText = article.price + "€"
 
       // Création des options de personnalisations des lentilles
       for (let i = 0; i < article.lenses.length; i++) {
@@ -40,6 +39,7 @@ function getCameras() {
     })
     // Création du message d'erreur
     .catch((error) => {
+      console.log(error);
       let container = document.querySelector(".container");
       container.innerHTML =
         "Nous n'avons pas réussi à afficher nos caméras. Si le problème persiste, contactez-nous.";
@@ -48,41 +48,63 @@ function getCameras() {
     });
 }
 
+// 
+function popUpConfirmation() {
+  if (window.confirm(`Votre produit a bien été ajouté au panier!
+      OK pour aller au panier
+      ou ANNULER pour continuer votre shopping!`)) {
+      window.location.href = "./cart.html";
+  } else {
+      window.location.href = "./index.html";
+  }
+}
+
 function addtoCart() {
-  const addtoCartBtn = document.querySelector(".add-to-cart");
+  const addtoCartBtn = document.querySelector(".add-cart-btn");
 
+  // Ecoute de l'événement au clic
   addtoCartBtn.addEventListener("click", () => {
-      if (cameraNumber.value > 0 && cameraNumber.value < 100) {
-          //Création du produit ajouté au panier
-          let productAdded = {
-              name: productCardName.innerHTML,
-              price: parseFloat(productCardPrice.innerHTML),
-              quantity: parseFloat(document.querySelector("#cameraNumber").value),
-              _id: id,
-          };
-          // Déclaration tableau pour gestion du localStorage
-          let arrayProductsInCart = [];
+    // Création des caractéristiques du produit ajouté au panier
+    let productAdded = {
+      name: productCardName.innerHTML,
+      price: parseFloat(productCardPrice.innerHTML),
+      quantity: parseFloat(document.querySelector("#cameraNumber").value),
+      _id: id,
+    }
+        
+    let quantity = parseFloat(document.querySelector("#cameraNumber").value);
+        
+    // Ajout dans le LS
+    let arrayProductsInCart = JSON.parse(localStorage.getItem("products"));
 
-          //Si localStorage existe, on récupère son contenu
-          if (localStorage.getItem("products") !==null) {
-              arrayProductsInCart = JSON.parse(localStorage.getItem("products"));
-          }
-          // On crée le localStorage avec le produit ajouté
-          arrayProductsInCart.push(productAdded);
-          localStorage.setItem("products", JSON.stringify(arrayProductsInCart));
-          console.log(localStorage);
-          alert('Votre article a bien été ajouté');
-      } else {
-          alert('La quantité doit être comprise entre 1 et 99')
-      }
+    // S'il n'y a pas de panier, je crée un tableau
+    if (!arrayProductsInCart) {
+      let arrayProductsInCart = [];
+      arrayProductsInCart.push(productAdded);
+      localStorage.setItem("products", JSON.stringify(arrayProductsInCart));
+      popUpConfirmation();
+
+    // Sinon, si j'ai un panier
+    // Je vérifie que n'ai pas d'objet dans le panier avant de l'ajouter
+    } else if(!arrayProductsInCart.some(p => p._id === productAdded._id)) {
+      arrayProductsInCart.push(productAdded);
+      localStorage.setItem("products", JSON.stringify(arrayProductsInCart));
+      popUpConfirmation();
+
+    // Sinon
+    // Si je l'ai dans mon panier, je filtre le produit par son _id et je mets à jour la quantité
+    } else {
+      arrayProductsInCart
+      .filter(p => p._id === productAdded._id)
+      .map(productAdded => productAdded.quantity = quantity + productAdded.quantity)
+      .push(productAdded);
+      localStorage.setItem("products", JSON.stringify(arrayProductsInCart));
+      popUpConfirmation();
+    };
   });
 }
 
-// Appel de la fonction
-main();
-
-function main() {
-  getCameras();
-  addtoCart();
-}
+// Appel des fonctions
+getCameras();
+addtoCart();
 
